72847fd1256fd2f40cdc53d7110729b57eda80bf
GeraSamayoa <gerasamayoa419@gmail.com>
2024-04-13 23:32:29 -0600

    conexion

--- InterRedBE/Program.cs
+++ InterRedBE/Program.cs
@@ -1,3 +1,6 @@
+using InterRedBE.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register DbContext with SQL Server
+builder.Services.AddDbContext<InterRedContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
