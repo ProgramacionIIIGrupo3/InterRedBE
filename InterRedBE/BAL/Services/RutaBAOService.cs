1fee093a790b4dd628aa2b76b8c0316f41de4521
GeraSamayoa <gerasamayoa419@gmail.com>
2024-04-27 21:19:04 -0600

    Se implementa API para obtener rutas, se agregan metodos de busqueda y recorrido en lista cuadrupleda

--- InterRedBE/BAL/Services/RutaBAOService.cs
+++ InterRedBE/BAL/Services/RutaBAOService.cs
@@ -20,11 +20,7 @@ namespace InterRedBE.BAL.Services
         public async Task<ListaEnlazadaDoble<(ListaEnlazadaDoble<Departamento>, double)>> EncontrarTodasLasRutasAsync(int idDepartamentoInicio, int idDepartamentoFin, int numeroDeRutas = 5)
         {
             var (redDepartamentos, distancias) = await _rutaService.CargarRutasAsync();
-            var todasLasRutas = new ListaEnlazadaDoble<(ListaEnlazadaDoble<Departamento>, double)>();
-            var rutaActual = new ListaEnlazadaDoble<int>();
-            var distanciaAcumulada = 0.0;
-
-            EncontrarRutasDFS(redDepartamentos.ObtenerNodo(idDepartamentoInicio), idDepartamentoFin, new ListaEnlazadaDoble<int>(), rutaActual, distanciaAcumulada, todasLasRutas, distancias, redDepartamentos);
+            var todasLasRutas = redDepartamentos.BuscarTodasLasRutas(idDepartamentoInicio, idDepartamentoFin, distancias);
 
             // Ordenar las rutas por distancia y tomar las primeras rutas
             var rutasOrdenadas = todasLasRutas.OrderBy(r => r.Item2);
@@ -40,55 +36,6 @@ namespace InterRedBE.BAL.Services
                 }
             }
             return resultado;
-        }
-
-        private void EncontrarRutasDFS(NodoCuadruple<Departamento> actual, int destino, ListaEnlazadaDoble<int> visitados, ListaEnlazadaDoble<int> rutaActual, double distanciaAcumulada, ListaEnlazadaDoble<(ListaEnlazadaDoble<Departamento>, double)> todasLasRutas, Dictionary<(int, int), double> distancias, ListaCuadruple<Departamento> redDepartamentos)
-        {
-            // Verifica si el nodo actual ya fue visitado en esta ruta
-            if (visitados.Contains(actual.Id))
-            {
-                return; // Evita ciclos dentro de la misma ruta de exploración
-            }
-
-            // Agrega el nodo actual a la ruta y marca como visitado
-            rutaActual.InsertarAlFinal(actual.Id);
-            visitados.InsertarAlFinal(actual.Id);
-
-            if (actual.Id == destino)
-            {
-                var ruta = new ListaEnlazadaDoble<Departamento>();
-                foreach (var id in rutaActual)
-                {
-                    ruta.InsertarAlFinal(redDepartamentos.ObtenerNodo(id).Dato);
-                }
-                todasLasRutas.InsertarAlFinal((ruta, distanciaAcumulada));
-            }
-            else
-            {
-                // Continúa con la exploración
-                foreach (var vecino in new[] { actual.Norte, actual.Sur, actual.Este, actual.Oeste })
-                {
-                    if (vecino != null && !visitados.Contains(vecino.Id))
-                    {
-                        var edgeKey = (actual.Id, vecino.Id);
-                        var nuevosVisitados = new ListaEnlazadaDoble<int>();
-                        foreach (var visitado in visitados)
-                        {
-                            nuevosVisitados.InsertarAlFinal(visitado);
-                        }
-                        var nuevaRutaActual = new ListaEnlazadaDoble<int>();
-                        foreach (var nodo in rutaActual)
-                        {
-                            nuevaRutaActual.InsertarAlFinal(nodo);
-                        }
-                        EncontrarRutasDFS(vecino, destino, nuevosVisitados, nuevaRutaActual, distanciaAcumulada + distancias[edgeKey], todasLasRutas, distancias, redDepartamentos);
-                    }
-                }
-            }
-
-            // Elimina el nodo actual de rutaActual y desmarca como visitado
-            rutaActual.EliminarDatoX(actual.Id);
-            visitados.EliminarDatoX(actual.Id);
         }
     }
 }
--- InterRedBE/UTILS/Services/ListaCuadruple.cs
+++ InterRedBE/UTILS/Services/ListaCuadruple.cs
@@ -18,7 +18,7 @@ namespace InterRedBE.UTILS.Services
             {
                 nodos[id] = new NodoCuadruple<T>(id, dato);
             }
-            // Si el nodo ya existe, no hace nada o maneja según tu lógica de negocio.
+            // Si el nodo ya existe
         }
         public NodoCuadruple<T> AgregarNodo(int id, T dato)
         {
@@ -73,6 +73,130 @@ namespace InterRedBE.UTILS.Services
             {
                 throw new KeyNotFoundException("El nodo con el ID proporcionado no existe.");
             }
+        }
+
+
+        public ListaEnlazadaDoble<NodoCuadruple<T>> BuscarRuta(int idInicio, int idFin)
+        {
+            var rutaEncontrada = new ListaEnlazadaDoble<NodoCuadruple<T>>();
+            var visitados = new ListaEnlazadaDoble<int>();
+            var nodoInicio = ObtenerNodo(idInicio);
+
+            BuscarRutaDFS(nodoInicio, idFin, visitados, rutaEncontrada);
+
+            return rutaEncontrada;
+        }
+
+        private void BuscarRutaDFS(NodoCuadruple<T> nodoActual, int idFin, ListaEnlazadaDoble<int> visitados, ListaEnlazadaDoble<NodoCuadruple<T>> rutaEncontrada)
+        {
+            visitados.InsertarAlFinal(nodoActual.Id);
+            rutaEncontrada.InsertarAlFinal(nodoActual);
+
+            if (nodoActual.Id == idFin)
+            {
+                return;
+            }
+
+            foreach (var vecino in new[] { nodoActual.Norte, nodoActual.Sur, nodoActual.Este, nodoActual.Oeste })
+            {
+                if (vecino != null && !visitados.Contains(vecino.Id))
+                {
+                    BuscarRutaDFS(vecino, idFin, visitados, rutaEncontrada);
+                    if (rutaEncontrada.Contains(nodoActual))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            rutaEncontrada.EliminarDatoX(nodoActual);
+            visitados.EliminarDatoX(nodoActual.Id);
+        }
+
+        public ListaEnlazadaDoble<NodoCuadruple<T>> RecorridoEnProfundidad(int idInicio)
+        {
+            var recorrido = new ListaEnlazadaDoble<NodoCuadruple<T>>();
+            var visitados = new ListaEnlazadaDoble<int>();
+            var nodoInicio = ObtenerNodo(idInicio);
+
+            RecorridoDFS(nodoInicio, visitados, recorrido);
+
+            return recorrido;
+        }
+
+        private void RecorridoDFS(NodoCuadruple<T> nodoActual, ListaEnlazadaDoble<int> visitados, ListaEnlazadaDoble<NodoCuadruple<T>> recorrido)
+        {
+            visitados.InsertarAlFinal(nodoActual.Id);
+            recorrido.InsertarAlFinal(nodoActual);
+
+            foreach (var vecino in new[] { nodoActual.Norte, nodoActual.Sur, nodoActual.Este, nodoActual.Oeste })
+            {
+                if (vecino != null && !visitados.Contains(vecino.Id))
+                {
+                    RecorridoDFS(vecino, visitados, recorrido);
+                }
+            }
+        }
+
+        public ListaEnlazadaDoble<(ListaEnlazadaDoble<T>, double)> BuscarTodasLasRutas(int idInicio, int idFin, Dictionary<(int, int), double> distancias)
+        {
+            var todasLasRutas = new ListaEnlazadaDoble<(ListaEnlazadaDoble<T>, double)>();
+            var rutaActual = new ListaEnlazadaDoble<int>();
+            var distanciaAcumulada = 0.0;
+            var visitados = new ListaEnlazadaDoble<int>();
+
+            BuscarRutasDFS(ObtenerNodo(idInicio), idFin, visitados, rutaActual, distanciaAcumulada, todasLasRutas, distancias);
+
+            return todasLasRutas;
+        }
+
+        private void BuscarRutasDFS(NodoCuadruple<T> actual, int destino, ListaEnlazadaDoble<int> visitados, ListaEnlazadaDoble<int> rutaActual, double distanciaAcumulada, ListaEnlazadaDoble<(ListaEnlazadaDoble<T>, double)> todasLasRutas, Dictionary<(int, int), double> distancias)
+        {
+            // Verifica si el nodo actual ya fue visitado en esta ruta
+            if (visitados.Contains(actual.Id))
+            {
+                return; // Evita ciclos dentro de la misma ruta de exploración
+            }
+
+            // Agrega el nodo actual a la ruta y marca como visitado
+            rutaActual.InsertarAlFinal(actual.Id);
+            visitados.InsertarAlFinal(actual.Id);
+
+            if (actual.Id == destino)
+            {
+                var ruta = new ListaEnlazadaDoble<T>();
+                foreach (var id in rutaActual)
+                {
+                    ruta.InsertarAlFinal(ObtenerNodo(id).Dato);
+                }
+                todasLasRutas.InsertarAlFinal((ruta, distanciaAcumulada));
+            }
+            else
+            {
+                // Continúa con la exploración
+                foreach (var vecino in new[] { actual.Norte, actual.Sur, actual.Este, actual.Oeste })
+                {
+                    if (vecino != null && !visitados.Contains(vecino.Id))
+                    {
+                        var edgeKey = (actual.Id, vecino.Id);
+                        var nuevosVisitados = new ListaEnlazadaDoble<int>();
+                        foreach (var visitado in visitados)
+                        {
+                            nuevosVisitados.InsertarAlFinal(visitado);
+                        }
+                        var nuevaRutaActual = new ListaEnlazadaDoble<int>();
+                        foreach (var nodo in rutaActual)
+                        {
+                            nuevaRutaActual.InsertarAlFinal(nodo);
+                        }
+                        BuscarRutasDFS(vecino, destino, nuevosVisitados, nuevaRutaActual, distanciaAcumulada + distancias[edgeKey], todasLasRutas, distancias);
+                    }
+                }
+            }
+
+            // Elimina el nodo actual de rutaActual y desmarca como visitado
+            rutaActual.EliminarDatoX(actual.Id);
+            visitados.EliminarDatoX(actual.Id);
         }
 
     }
